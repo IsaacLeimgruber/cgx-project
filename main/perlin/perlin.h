5651617c234f4c59a3bac4a8f4815fdbaae77a46
Julien Harbulot <julien.harbulot@epfl.ch>
2017-04-05 19:39:52 +0200

    Cache and make permutations random in perlin-noise

--- main/perlin/perlin.h
+++ main/perlin/perlin.h
@@ -1,9 +1,11 @@
 #pragma once
+#include <cstdlib>
+#include <random>
 #include "icg_helper.h"
 
 class Perlin {
 
-    private:
+private:
         GLuint vertex_array_id_;        // vertex array object
         GLuint program_id_;             // GLSL shader program ID
         GLuint vertex_buffer_object_;   // memory buffer
@@ -12,8 +14,47 @@ class Perlin {
         float screenquad_width_;
         float screenquad_height_;
 
-    public:
+        int p[512];
+
+public:
+        void initializePermutations(unsigned rand_seed){
+
+            // create a permutation array with Perlin's recommanded values
+            // these values won't be used and are here for reference only
+            int permutation[256]{
+                151,160,137,91,90,15,
+                131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
+                190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
+                88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
+                77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
+                102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
+                135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
+                5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
+                223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
+                129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
+                251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
+                49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
+                138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
+            };
+
+            // overwrites permutation array with random numbers
+            // we can change the distribution for different result
+            std::minstd_rand0 gen(rand_seed);
+            std::uniform_int_distribution<> dis(0, 250);
+            for (int i = 0; i < 256; ++i) {
+                permutation[i] = dis(gen);
+                cout << permutation[i] << " ";
+            }
+
+            // duplicate the array into `p`
+            for (int i = 0; i < 512; ++i) {
+                p[i] = permutation[i%256];
+            }
+        }
+
         void Init() {
+
+            initializePermutations(time(NULL));
 
             // compile the shaders
             program_id_ = icg_helper::LoadShaders("perlin_vshader.glsl",
@@ -69,8 +110,10 @@ class Perlin {
                                       ZERO_BUFFER_OFFSET);
             }
 
+
             // to avoid the current object being polluted
             glBindVertexArray(0);
+            glUniform1iv(glGetUniformLocation(program_id_, "p"), 512, p);
             glUseProgram(0);
         }
 
@@ -83,10 +126,10 @@ class Perlin {
             glDeleteTextures(1, &texture_id_);
         }
 
-        void Draw() {
+        void Draw(std::vector<GLfloat> pos_offset = {0.5f, 0.5f}) {
             glUseProgram(program_id_);
+            glUniform1fv(glGetUniformLocation(program_id_, "pos_offset"), 1, pos_offset.data());
             glBindVertexArray(vertex_array_id_);
-
             // bind texture
             glActiveTexture(GL_TEXTURE0);
             glBindTexture(GL_TEXTURE_2D, texture_id_);
--- main/perlin/perlin_vshader.glsl
+++ main/perlin/perlin_vshader.glsl
@@ -3,6 +3,7 @@ in vec3 vpoint;
 in vec2 vtexcoord;
 out vec2 uv;
 
+
 void main() {
     gl_Position = vec4(vpoint, 1.0);
     uv = vtexcoord;
