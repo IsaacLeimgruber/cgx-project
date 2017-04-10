456232ec6b145da59f2f0bcb3574194c3364be01
lkieliger <leandro.kieliger@epfl.ch>
2017-04-10 09:05:17 +0200

    Prepare branch for merge

--- main/main.cpp
+++ main/main.cpp
@@ -206,7 +206,7 @@ int main(int argc, char *argv[]) {
 
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
